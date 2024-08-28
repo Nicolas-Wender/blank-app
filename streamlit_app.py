9bb2b6caf1d9029c7bede566edd5afd1ee6c3fa7
nicolas <nicolas.w3nder@gmail.com>
2024-08-28 09:12:19 -0300

    mudando a query

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ def run_query(query):
     rows = [dict(row) for row in rows_raw]
     return rows
 
-rows = run_query("SELECT word FROM `bigquery-public-data.samples.shakespeare` LIMIT 10")
+rows = run_query("SELECT * FROM `integracao-414415.data_ptl.produtos_bling_proteloja` LIMIT 10")
 
 # Print results.
 st.write("Some wise words from Shakespeare:")
